88342d1264e89894cc0ffdd967fd05ddef2fcff8
Kai De La Cruz <kaidcruz@gmail.com>
2025-11-19 11:29:13 -0800

    rqt visuals

--- CLdev.sh
+++ CLdev.sh
@@ -1,5 +1,5 @@
-sudo chmod a+rw /dev/i2c-7
-sudo chmod a+rw /dev/ttyUSB0
+# sudo chmod a+rw /dev/i2c-7
+# sudo chmod a+rw /dev/ttyUSB0
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 # colcon build --packages-select quadruped_hardware
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
--- CLnew.sh
+++ CLnew.sh
@@ -1,6 +1,8 @@
 # sudo chmod a+rw /dev/ttyACM0
 # sudo chmod a+rw /dev/ttyUSB0
-sudo chmod a+rw /dev/i2c-7
+# sudo chmod a+rw /dev/i2c-7
 source /opt/ros/jazzy/setup.bash
-export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+# export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 source install/setup.bash
+export DISPLAY=host.docker.internal:0.0
+export QT_X11_NO_MITSHM=1
