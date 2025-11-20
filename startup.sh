2aa35be1f4d2b7cdf6214157372670926712c6e5
Kai De La Cruz <kaidcruz@gmail.com>
2025-11-20 14:42:09 -0800

    startup bash

--- startup.sh
+++ startup.sh
@@ -3,6 +3,8 @@
 # sudo chmod a+rw /dev/i2c-7
 source /opt/ros/jazzy/setup.bash
 # export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+cd /home/ws
+colcon build
 source install/setup.bash
 export DISPLAY=host.docker.internal:0.0
 export QT_X11_NO_MITSHM=1
